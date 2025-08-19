16ab795dcf568449e466e7ffda4b88a06735254a
Matheus Marchetti <matheusmarchetti1502@gmail.com>
2025-08-18 22:24:33 -0300

    CRUD

--- routes/livrosRoutes.js
+++ routes/livrosRoutes.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const route = express.Router();
-const livrosController =
-  require('../controllers/livrosController');
+const livrosController = require("../controllers/livrosController");
 
-route.get('/', livrosController.listar);
+route.get("/", livrosController.listar);
+route.get("/:id/editar", livrosController.editar);
+route.get("/create", livrosController.create);
+route.post("/", livrosController.insert);
+route.post("/:id", livrosController.atualizar);
 
 module.exports = route;
